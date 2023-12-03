9ac5c6f5b32a4424ce92a9aacce6bc29213ee8e8
Radostin Ivanov <raven_95@mail.bg>
2023-12-03 15:18:22 +0200

    Added Welcome Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import React from 'react';
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import Home from './Home';
+// import Login from './Login';
+// import Register from './Register';
+// import CharacterCreate from './CharacterCreate';
+// import Arena from './Arena';
 import './App.css'
+import WelcomePage from './components/welcome-page/WelcomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <WelcomePage />
   )
-}
 
+}
 export default App
---
+++ client/src/components/welcome-page/WelcomePage.jsx
@@ -0,0 +1,11 @@
+export default function WelcomePage() {
+
+    return (
+        <section id="welcome-page" className="auth">
+            <h1>Welcome, if you want to proceed, you must first...</h1>
+            <button>Log in</button>
+            <p>or</p>
+            <button>Register</button>
+        </section>
+    );
+}
