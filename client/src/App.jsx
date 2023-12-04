2b2411b9012f090c43d1d77380ffd87278b95a17
Radostin Ivanov <raven_95@mail.bg>
2023-12-04 20:36:39 +0200

    reorganized app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
-import React, { useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router';
-
-import * as authService from './services/authService';
-import AuthContext from './contexts/authContext';
+import React, { Suspense, } from 'react';
+import { Route, Routes } from 'react-router';
+import { AuthProvider } from './contexts/authContext';
 
 import WelcomePage from './components/welcome-page/WelcomePage';
 import CharacterCreate from './components/character-create/CharacterCreate';
@@ -12,62 +10,27 @@ import Register from './components/register/Register';
 import Header from './components/header/Header';
 import Arena from './components/arena/Arena';
 import Logout from './components/logout/Logout';
+import ErrorBoundary from './components/ErrorBoundary';
 
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken');
-    return {};
-  });
-
-  const loginSubmitHandler = async (values) => {
-    const result = await authService.login(values.email, values.password)
-
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-
-    navigate('/')
-  }
-
-  const registerSubmitHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-
-    navigate('/')
-  };
-
-  const logoutHandler = () => {
-    setAuth({});
-    localStorage.removeItem('accessToken');
-  };
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  }
-
   return (
+    <ErrorBoundary>
+      <AuthProvider>
+        <Header />
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/character-create" element={<CharacterCreate />} />
+            <Route path="/logout" element={<Logout />} />
 
-    <AuthContext.Provider value={values}>
-      <Header />
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/character-create" element={<CharacterCreate />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/arena" element={<Arena />} />
-
-      </Routes>
-
-    </AuthContext.Provider>
-
+            <Route path="/arena" element={<Arena />} />
+          </Routes>
+        </Suspense>
+      </AuthProvider>
+    </ErrorBoundary>
   )
 
 }
