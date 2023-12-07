ae89f4c7b453b6c41f87c010709572df57975fae
Radostin Ivanov <raven_95@mail.bg>
2023-12-07 21:51:20 +0200

    Update Arena.jsx

--- client/src/components/arena/Arena.jsx
+++ client/src/components/arena/Arena.jsx
@@ -35,7 +35,8 @@ export default function Arena({
 
   const handleUserCharacterSelect = (character) => {
     setSelectedUserCharacter(character);
-    useEffect
+   
+
   };
   const handleOpponentCharacterSelect = (character) => {
     setSelectedOpponentCharacter(character);
